1cd2958e0815fc3290cf2f36e50e6e70b0bd00c6
yuzhouheike <26108724@qq.com>
2016-05-09 18:52:15 +0800

    初始化代码

---
+++ 10086/10086/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  10086
+//
+//  Created by ma c on 16/5/6.
+//  Copyright © 2016年 王磊磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
